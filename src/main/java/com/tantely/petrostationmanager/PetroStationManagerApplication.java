dfb1c13bf76a8e9c2a6b5d2a90dd5062cd3590a8
Hevitriniavo <hei.tantely@gmail.com>
2024-05-06 11:48:11 +0545

    chore: initialized project

---
+++ src/main/java/com/tantely/petrostationmanager/PetroStationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tantely.petrostationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetroStationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetroStationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tantely/petrostationmanager/PetroStationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tantely.petrostationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetroStationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
