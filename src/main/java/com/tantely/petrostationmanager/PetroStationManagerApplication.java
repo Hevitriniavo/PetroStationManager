6c920b29c3613e4f015f3a574fdd0f94ace7e0d3
Hevitriniavo <hei.tantely@gmail.com>
2024-05-07 14:15:30 +0545

    feat: configure/postgresql

--- src/main/java/com/tantely/petrostationmanager/PetroStationManagerApplication.java
+++ src/main/java/com/tantely/petrostationmanager/PetroStationManagerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetroStationManagerApplication {
 
-	private String name;
 	public static void main(String[] args) {
 		SpringApplication.run(PetroStationManagerApplication.class, args);
 	}
